bcb0f7a335ba0bfffcc7f8e0726068fe1cb98ce5
rafa1um <rafael.rxp1@gmail.com>
2018-08-31 09:21:18 -0300

    S0 FAR, S0 G00D... S0 WHAT!

--- t3-modulo/src/svg.c
+++ t3-modulo/src/svg.c
@@ -15,28 +15,6 @@ void circ(FILE* output, REGISTRO *regCirculo) {
   fprintf(output, "\t<circle fill='%s' stroke='%s' r='%f' cx='%f' cy='%f' />\n", regCirculo->cor, regCirculo->borda, regCirculo->r, regCirculo->x, regCirculo->y);
 }
 
-void quadra_SVG(FILE* output, REGISTRO *reg_quadra) {
-  fprintf(output, "\t<rect fill='%s' stroke='%s' stroke-width='%dpx' width='%f' height='%f' y='%f' x='%f' />\n", reg_quadra->cor, reg_quadra->borda, 1, reg_quadra->largura, reg_quadra->altura, reg_quadra->y, reg_quadra->x);
-  fprintf(output, "\t<text x='%f' y='%f' font-family='Arial' font-size='10' fill='%s'>%s</text>\n", (reg_quadra->x)+45, (reg_quadra->y)+40, "black", reg_quadra->id);
-}
-
-/* DESENHA UM HIDRANTE NO ARQUIVO SVG */
-void hidrante_SVG(FILE* output, REGISTRO *reg_hidrante) {
-  fprintf(output, "\t<circle fill='%s' stroke='%s' r='8px' cx='%f' cy='%f' />\n", reg_hidrante->cor, reg_hidrante->borda, reg_hidrante->x, reg_hidrante->y);
-  fprintf(output, "\t<text x='%f' y='%f' font-family='Arial' font-size='10' fill='%s'>%c</text>\n", (reg_hidrante->x)-3.5, (reg_hidrante->y)+3, "white", 'H');
-}
-
-/* DESENHA UM SEMAFORO NO ARQUIVO SVG */
-void semaforo_SVG(FILE* output, REGISTRO *reg_semaforo) {
-  fprintf(output, "\t<rect fill='%s' stroke='%s' stroke-width='20' width='15px' height='35px' y='%f' x='%f' />\n", reg_semaforo->cor, reg_semaforo->borda, reg_semaforo->y, reg_semaforo->x);
-  fprintf(output, "\t<text x='%f' y='%f' font-family='Arial' font-size='10' fill='%s'>%c</text>\n", (reg_semaforo->x)+4, (reg_semaforo->y)+20, "black", 'S');
-}
-
-void torre_SVG(FILE* output, REGISTRO *reg_torre) {
-  fprintf(output, "\t<circle fill='%s' stroke='%s' r='8px' cx='%f' cy='%f' />\n", reg_torre->cor, reg_torre->borda, reg_torre->x, reg_torre->y);
-  fprintf(output, "\t<text x='%f' y='%f' font-family='Arial' font-size='10' fill='%s'>%c</text>\n", (reg_torre->x)-3.5, (reg_torre->y)+3, "white", 'T');
-}
-
 void line(FILE* o_suSVG, NODE search01, NODE search02, double x, double y) {
     while(search02 != NULL) {
       if(search02->reg.r > 0){
